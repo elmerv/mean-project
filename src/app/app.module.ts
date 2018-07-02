4ce3e6784ff21c6c6299cbd7c4276a065d043a38
elmerv <vasquezelmer101@gmail.com>
2018-07-01 23:32:19 -0700

    Ability to add user input into separate posts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import { MatInputModule, MatCardModule, MatButtonModule } from '@angular/material';
+import { MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule, MatExpansionModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import {PostCreateComponent} from './posts/posts-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './post-list/post-list.component';
 @NgModule({
   declarations: [
-    AppComponent, 
+    AppComponent,
     PostCreateComponent,
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
